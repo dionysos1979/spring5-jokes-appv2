d99d0ae6fb97a9283d95de380466635f6167be7f
dionysos1979 <vincent.drouhin@gmail.com>
2022-12-16 15:48:46 +0100

    Init project

---
+++ src/main/java/com/traderpatient/spring5jokesappv2/Spring5JokesAppv2Application.java
@@ -0,0 +1,13 @@
+package com.traderpatient.spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppv2Application.class, args);
+    }
+
+}
